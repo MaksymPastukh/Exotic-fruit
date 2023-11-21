adc4f63c7dead5ef224ee4d0067cc8ff3f2b0ab5
MaksymPastukh <saponexamq@gmail.com>
2023-11-21 21:29:47 +0100

    commit update

--- scripts/script.js
+++ scripts/script.js
@@ -24,7 +24,7 @@ function tabs(headerSelector, tabSelector, contentSelector, activeClass, display
         if (target.classList.contains(tabSelector.replace(/\./, '')) ||
             target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
             tab.forEach((item, i) => {
-                if (target == item || target.parentNode == item) {
+                if (target === item || target.parentNode === item) {
                     hideTabContent()
                     showTabContent(i)
                 }
@@ -33,62 +33,16 @@ function tabs(headerSelector, tabSelector, contentSelector, activeClass, display
     })
 }
 
-// Аккордеон
-function accordion() {
-    const items = document.querySelectorAll('.accordion-item-trigger')
-    items.forEach(item => {
-        item.addEventListener('click', () => {
-            const parent = item.parentNode
-            if (parent.classList.contains('accordion-item-active')) {
-
-                parent.classList.remove('accordion-item-active')
-            } else {
-                document
-                    .querySelectorAll('.accordion-item')
-                    .forEach(child => child.classList.remove('accordion-item-active'),
-                        )
-                parent.classList.add('accordion-item-active')
-            }
-        })
-    })
-}
-
-function popup() {
-    const popup = document.querySelector('.popup'),
-        closeOk = document.querySelectorAll('.btn-popup'),
-        closePopup = document.querySelector('.close-popup');
-
-    closeOk.forEach(item => {
-        item.addEventListener('click', () => {
-            popup.style.animation = "jackInTheBox 1s ease";
-            popup.style.display = "block";
-            document.body.style.overflow = "";
-        })
-    })
 
 
-    popup.addEventListener('click', (e) => {
-        if (e.target === popup) {
-            popup.style.display = "none";
-            document.body.style.overflow = "";
-        }
-    })
 
-    closePopup.addEventListener('click', () => {
-        popup.style.display = 'none';
-        document.body.overflow = '';
-    })
-
-}
-
-
-accordion()
+// accordion()
 // popup()
 // ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
 // ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
 // ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
 // ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
-tabs('.tabs-header', '.tabs-header-item', '.tabs-content-item', 'active')
+// tabs('.tabs-header', '.tabs-header-item', '.tabs-content-item', 'active')
 
 // let slideIndex = 1;
 // const slides = document.querySelectorAll('.item-slider'),
